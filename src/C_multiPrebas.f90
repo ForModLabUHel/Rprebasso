f0125ab64154a80bb7b8fb1b9721c091a37b7438
checcomi <checcomi@gmail.com>
2020-02-08 18:14:12 +0000

    rev previous2

--- src/C_multiPrebas.f90
+++ src/C_multiPrebas.f90
@@ -47,8 +47,8 @@ soilCtot = soilCtotInOut
 do i = 1,nSites
  do ijj = 1,nLayers(i)
 	species = int(initVar(i,1,ijj))
-		initVar(i,7,ijj) = pCrobas(38,species)/pCrobas(15,species) * (initVar(i,3,ijj) -&
-			initVar(i,6,ijj))**pCrobas(11,species)!A = p_ksi/p_rhof * Lc^p_z
+		initVar(i,7,ijj) = max(0.,pCrobas(38,species)/pCrobas(15,species) * (initVar(i,3,ijj) -&
+			initVar(i,6,ijj))**pCrobas(11,species))!A = p_ksi/p_rhof * Lc^p_z
 	call initBiomasses(pCrobas(:,species),initVar(i,:,ijj),siteInfo(i,3),multiOut(i,1,:,ijj,1))
  enddo
 enddo
