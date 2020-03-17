e6af4ece93064476f5270d1d77b2f19f1d0fec3d
checcomi <checcomi@gmail.com>
2020-03-17 10:24:13 +0000

    litterSizeDef fixed

--- R/utilStaff.r
+++ R/utilStaff.r
@@ -61,11 +61,11 @@ parsAWEN[,4] <- c(0.407900,0.198000,0.099000,0.295100,0.474660,0.019012,
                   0.078308,0.430248,0.715000,0.015000,0.000000,0.275000)
 
 ##litterSize default values##
-litterSize <- matrix(NA,3,4,dimnames = list(c("CwoodLit","FwoodLit","NwoodLit"),c("pisy","piab","beal","eugl")))
-litterSize[,1] <- c(30,2,0)
-litterSize[,2] <- c(30,2,0)
-litterSize[,3] <- c(10,2,0)
-litterSize[,4] <- c(20,2,0)
+litterSizeDef <- matrix(NA,3,4,dimnames = list(c("CwoodLit","FwoodLit","NwoodLit"),c("pisy","piab","beal","eugl")))
+litterSizeDef[,1] <- c(30,2,0)
+litterSizeDef[,2] <- c(30,2,0)
+litterSizeDef[,3] <- c(10,2,0)
+litterSizeDef[,4] <- c(20,2,0)
 
 ClCut_birch <- matrix(NA,2,4)
 ClCut_birch[1,] <- c(30.0,60,28.5,60)
