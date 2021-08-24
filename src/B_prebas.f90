2312fc9b9a96d8d0e31c56647dcaaee473773781
ForModLabUHel <francesco.minunno@helsinki.fi>
2021-08-24 16:09:59 +0100

    test

--- src/B_prebas.f90
+++ src/B_prebas.f90
@@ -1140,7 +1140,7 @@ if (ClCut == 1.) then
 	A_clearcut = inAclct(species)
 	D = stand_all(12,layer)
 	age = stand_all(7,layer)
-	write(1,*) siteInfo(1),D, D_clearcut,age > A_clearcut
+	write(1,*) siteInfo(1),D, D_clearcut,D > D_clearcut,age, A_clearcut , age > A_clearcut
  if ((D > D_clearcut) .and. (age > A_clearcut)) then
   ! modOut(year+1,1,2,2) = 1. !flag for clearcut
   thinClx(year,2) = 1 !flag for clearcut
