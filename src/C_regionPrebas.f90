812ead8247682fdc11817f602cc0cdd6669f8dd3
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-01-18 17:40:23 +0000

    update

--- src/C_regionPrebas.f90
+++ src/C_regionPrebas.f90
@@ -205,7 +205,7 @@ do ij = 1,maxYears
 	if(flagFert(i)==1) then 
 		yearsFert = max(1,min(((nYears(i)) - ij-1),nYearsFert))
 		multiOut(i,(ij+1):(ij+yearsFert),3,:,1) = siteInfo(i,3)-1.
-		flagFert(i)==2
+		flagFert(i)=2
 		!write(*,*)siteInfo(i,3),yearsFert,multiOut(i,(ij+1):(ij+yearsFert),3,1,1)
 	endif
 
