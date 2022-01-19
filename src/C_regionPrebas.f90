1a5cd3229f627f5dfb40e379d47036892a54087a
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-01-19 19:14:41 +0000

    step

--- src/C_regionPrebas.f90
+++ src/C_regionPrebas.f90
@@ -206,11 +206,9 @@ do ij = 1,maxYears
 	if(flagFert(i)==1) then 
 		yearsFert = max(1,min(((nYears(i)) - ij-1),nYearsFert))
 		multiOut(i,(ij+1):(ij+yearsFert),3,:,1) = siteInfo(i,3)-1.
-
-		! call calcAlfar(multiOut(i,ij,3,:,:),multiOut(i,ij,4,:,1),pCrobas, &
-				! maxNlayers,alfarFert,allSP,nYearsFert,npar)
-		! multiOut(i,(ij+1):(ij+yearsFert),3,:,2) = alfarFert(1:yearsFert,:)
-		
+		call calcAlfar(multiOut(i,ij,3,:,:),multiOut(i,ij,4,:,1),pCrobas, &
+				nLayers(i),alfarFert,allSP,nYearsFert,npar)
+		multiOut(i,(ij+1):(ij+yearsFert),3,:,2) = alfarFert(1:yearsFert,:)
 		flagFert(i)=2
 	endif
 
