4de2b1f6b9f18685b46020c95caf8d6bdb8932fd
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-02-15 16:27:18 +0000

    update

--- src/C_regionPrebas.f90
+++ src/C_regionPrebas.f90
@@ -78,7 +78,7 @@ multiOut(:,1,7,:,1) = initVar(:,2,:) !initialize age used in the mitigation scen
     ! open(1,file="test1.txt")
     ! open(2,file="test2.txt")
     ! open(3,file="test3.txt")
-  ! open(1,file="test.txt")
+  open(1,file="test.txt")
  ! open(2,file="tTapioReg.txt")
  ! write(1,*) ftTapio
  ! write(1,*) tTapio
@@ -328,11 +328,16 @@ if(oldLayer==1) then
 	 multiOut(siteX,ij,(/9,10,13,17,18,37,38,40,43,44,53,54/),layerX,1) * (pDomRem)
 	multiOut(siteX,ij,24:34,nLayers(siteX),1) = multiOut(siteX,ij,24:34,layerX,1) * (pDomRem)
 	multiOut(siteX,ij,47:51,nLayers(siteX),1) = multiOut(siteX,ij,47:51,layerX,1) * (pDomRem)
+    !!!if oldLayer is active import siteType and alfar from the single site simulations simulations
+	multiOut(siteX,ij:maxYears,3,nLayers(siteX),1) = multiOut(siteX,ij,3,layerX,1)
+	multiOut(siteX,ij:maxYears,3,nLayers(siteX),2) = multiOut(siteX,ij,3,layerX,2)
+	write(1,*) multiOut(siteX,ij,3,nLayers(siteX),1), multiOut(siteX,ij,3,nLayers(siteX),2), "test1"
 	!update dominant layer
 	multiOut(siteX,ij,(/9,10,13,17,18,37,38,40,43,44,53,54/),layerX,1) = &
  	 multiOut(siteX,ij,(/9,10,13,17,18,37,38,40,43,44,53,54/),layerX,1) * (1-pDomRem)
 	multiOut(siteX,ij,24:34,layerX,1) = multiOut(siteX,ij,24:34,layerX,1) * (1-pDomRem)
 	multiOut(siteX,ij,47:51,layerX,1) = multiOut(siteX,ij,47:51,layerX,1) * (1-pDomRem)
+
 else
  jj=nLayers(siteX)
 endif
@@ -564,6 +569,10 @@ if(oldLayer==1) then
 	 multiOut(siteX,ij,(/9,10,13,17,18,37,38,40,43,44,53,54/),layerX,1) * (pDomRem)
 	multiOut(siteX,ij,24:34,nLayers(i),1) = multiOut(siteX,ij,24:34,layerX,1) * (pDomRem)
 	multiOut(siteX,ij,47:51,nLayers(i),1) = multiOut(siteX,ij,47:51,layerX,1) * (pDomRem)
+    !!!if oldLayer is active import siteType and alfar from the single site simulations simulations
+	multiOut(siteX,ij:maxYears,3,nLayers(siteX),1) = multiOut(siteX,ij,3,layerX,1)
+	multiOut(siteX,ij:maxYears,3,nLayers(siteX),2) = multiOut(siteX,ij,3,layerX,2)
+	write(1,*) multiOut(siteX,ij,3,nLayers(siteX),1), multiOut(siteX,ij,3,nLayers(siteX),2), "test2"
 	!update dominant layer
 	multiOut(siteX,ij,(/9,10,13,17,18,37,38,40,43,44,53,54/),layerX,1) = &
  	 multiOut(siteX,ij,(/9,10,13,17,18,37,38,40,43,44,53,54/),layerX,1) * (1-pDomRem)
@@ -806,7 +815,7 @@ end do !end Year loop
     enddo !ijj
   enddo
  enddo	
-  ! close(1)
+  close(1)
   ! close(2)
   ! close(3)
 ! write(10,*) "here5"
