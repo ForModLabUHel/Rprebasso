802b193779f67b3f84c4d1c300c1c9c3d6d3c9d5
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-02-21 21:52:48 +0000

    test

--- R/multiSitePrebas.r
+++ R/multiSitePrebas.r
@@ -476,9 +476,15 @@ regionPrebas <- function(multiSiteInit,
     sitesCl1 <- which(multiSiteInit$siteInfo[,3]<4)
     sitesCl2 <- which(multiSiteInit$siteInfo[,3]>3)
     siteOrder1 <- matrix(sitesCl1,length(sitesCl1),multiSiteInit$maxYears)
-    siteOrder1 <- apply(siteOrder1,2,sample)
     siteOrder2 <- matrix(sitesCl2,length(sitesCl2),multiSiteInit$maxYears)
-    siteOrder2 <- apply(siteOrder2,2,sample)
+    for(ix in 1: multiSiteInit$maxYears){
+      siteOrder1[,ix] <- sample(sitesCl1)
+      siteOrder2[,ix] <- sample(sitesCl2)
+    }
+    # siteOrder1 <- matrix(sitesCl1,length(sitesCl1),multiSiteInit$maxYears)
+    # siteOrder1 <- apply(siteOrder1,1,sample)
+    # siteOrder2 <- matrix(sitesCl2,length(sitesCl2),multiSiteInit$maxYears)
+    # siteOrder2 <- apply(siteOrder2,1,sample)
     siteOrder <- rbind(siteOrder1,siteOrder2)
   }else if(all(is.na(siteOrder))){
     siteOrder <- matrix(1:multiSiteInit$nSites,multiSiteInit$nSites,multiSiteInit$maxYears)
