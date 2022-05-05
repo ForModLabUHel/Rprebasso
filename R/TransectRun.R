c5f16409cb1b2cc7646fad5ad7287ec8adc63f66
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-05-05 17:19:37 +0100

    update

--- R/TransectRun.R
+++ R/TransectRun.R
@@ -23,7 +23,7 @@
 #' 
 TransectRun <- function(SiteType = NA, initVar = NA, species = NA, nYears = 100, pCROBAS = pCROB,
                         AgeEffect = F, defaultThin = 1, ClCut = 1, energyCut = 0,
-                        mortMod=1) {
+                        mortMod=1,GVrun=1) {
   nSites <- 7
   siteInfo <- matrix(c(NA, NA, NA, 160, 0, 0, 20, 3, 3, 413, 0.45, 0.118), nSites, 12, byrow = T)
   if (is.na(SiteType)) {
@@ -135,7 +135,8 @@ TransectRun <- function(SiteType = NA, initVar = NA, species = NA, nYears = 100,
     ClCut = ClCut,
     yassoRun = 1,
     energyCut = energyCut,
-    mortMod=mortMod
+    mortMod=mortMod,
+    GVrun=GVrun
   )
   initPrebas$multiInitVar[, 2, ] <- initialAgeSeedl(initPrebas$siteInfo[, 3], rowMeans(initPrebas$ETS)) # Initial age
   TransectOut <- multiPrebas(initPrebas)
