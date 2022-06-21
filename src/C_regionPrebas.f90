a69eb13dc74c3b7d9496c5ed1a02d4d8cc845fc3
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-06-22 01:07:41 +0300

    test

--- src/C_regionPrebas.f90
+++ src/C_regionPrebas.f90
@@ -77,7 +77,7 @@ multiOut(:,1,7,:,1) = initVar(:,2,:) !initialize age used in the mitigation scen
 multiOut(:,1,4,:,1) = initVar(:,1,:) !initialize species 
 
     ! open(1,file="test1.txt")
-    ! open(2,file="test2.txt")
+    open(2,file="test2.txt")
     ! open(3,file="test3.txt")
 
 ! write(2,*) "compHarv",compHarv
@@ -526,6 +526,9 @@ endif
  	  initVar(siteX,2,ijj) = multiOut(siteX,ij,7,ijj,1)
 	  initVar(siteX,3:6,ijj) = multiOut(siteX,ij,11:14,ijj,1)
 	  initVar(siteX,7,ijj) = multiOut(siteX,ij,16,ijj,1)
+if(siteInfo(siteX,1) == 454702.) write(2,*) "thinned", ij, stand_all(7,ij), outt(11,ij,2) , outt(13,ij,2) , outt(30,ij,2) 
+if(siteInfo(siteX,1) == 454702.) write(2,*) "remaining", ij, stand_all(7,ij), stand_all(11,ij),stand_all(13,ij) ,stand_all(30,ij) 
+
      enddo !ijj layers loop
 	 
  	!!!if fertilization at thinning is active,  decrease siteType
@@ -844,7 +847,7 @@ end do !end Year loop
   enddo
  enddo	
   ! close(1)
-  ! close(2)
+  close(2)
   ! close(3)
 soilCinOut = soilC
 soilCtotInOut = soilCtot
