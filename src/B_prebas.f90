0e9e610e9e4de6da250f33807a747752485fd2d6
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-07-14 14:13:47 +0100

    update

--- src/B_prebas.f90
+++ src/B_prebas.f90
@@ -716,7 +716,7 @@ if (N>0.) then
 			if(ECMmod==1) then !!!ECMmodelling
 				call CUEcalc(ETS, sitetype,par_mr,W_froot,r_RT,rm_aut_roots,litt_RT,exud(ij)) !!!ECMmodelling
 				! write(1,*) r_RT,rm_aut_roots,litt_RT,exud(ij)
-				Respi_m = par_mf * wf_STKG + par_mw * W_wsap + rm_aut_roots  !!!ECMmodelling
+				Respi_m = par_mf * wf_STKG + par_mw * W_wsap + rm_aut_roots * W_froot  !!!ECMmodelling
 				! write(2,*) r_RT,rm_aut_roots,litt_RT,exud(ij)
 			else !!!ECMmodelling
 				Respi_m = (par_mf + par_alfar*par_mr)* wf_STKG + par_mw * W_wsap  !!newX
