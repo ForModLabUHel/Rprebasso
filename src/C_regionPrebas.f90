8e3b581b3f7f9ef6ad4ddea577ebaa301c45fa89
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-09-06 18:02:23 +0100

    Update C_regionPrebas.f90

--- src/C_regionPrebas.f90
+++ src/C_regionPrebas.f90
@@ -14,6 +14,7 @@ subroutine regionPrebas(siteOrder,HarvLim,minDharv,multiOut,nSites,areas,nClimID
 implicit none
 
 integer, parameter :: nVar=54,npar=47!, nSp=3
+real (kind=8), parameter :: pi = 3.1415927
 real (kind=8), parameter :: harvRatio = 0.9, energyRatio = 0.7
 integer, intent(in) :: nYears(nSites),nLayers(nSites),allSP,oldLayer
 integer :: i,climID,ij,iz,ijj,ki,n,jj,az
