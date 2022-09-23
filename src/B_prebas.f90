bce21ed5efbed0f326342c3e34cc85bc1e464dce
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-09-23 09:23:15 +0100

    test

--- src/B_prebas.f90
+++ src/B_prebas.f90
@@ -1552,7 +1552,7 @@ call fAPARgv(fAPARtrees, ETSmean, 3., lastGVout(1), lastGVout(2), &
 	   ! write(1,*) siteInfo(1), lastGVout
   ! close(1)
   ! endif
-  GVout(nYears,5) = 2000. - GVout((nYears),4)/10.d0 + GVout((nYears),2)/10.d0
+  ! GVout(nYears,5) = lastGVout(4)/10.d0 - GVout((nYears),4)/10.d0 + GVout((nYears),2)/10.d0
   ! if(isnan(GVout(nYears,5))) then
 	   ! write(1,*) siteInfo(1), lastGVout)
   ! close(1)
