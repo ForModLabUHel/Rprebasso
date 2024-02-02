5cda8650a6d473ae0f292b5a5ff4ba005b5fd27d
ForModLabUHel <francesco.minunno@helsinki.fi>
2024-02-02 14:30:31 +0000

    typo

--- R/multiSitePrebas.r
+++ R/multiSitePrebas.r
@@ -667,12 +667,12 @@ multiPrebas <- function(multiSiteInit,
   ####initialize disturbance module if exists
   if(all(is.na(siteInfoDist))){
     disturbanceON = FALSE
-    siteInfoDist = matrix(0,nSites,4)
-    outDist = array(0,dim=c(nSites,max(nYearsMS),10))
+    siteInfoDist = matrix(0,multiSiteInit$nSites,4)
+    outDist = array(0,dim=c(multiSiteInit$nSites,max(nYearsMS),10))
   }else{
     disturbanceON = TRUE
     #siteInfoDist = matrix(0,nSites,4)
-    outDist = array(0,dim=c(nSites,max(nYearsMS),10))
+    outDist = array(0,dim=c(multiSiteInit$nSites,max(nYearsMS),10))
   }
   
   
@@ -801,12 +801,12 @@ regionPrebas <- function(multiSiteInit,
   ####initialize disturbance module if exists
   if(all(is.na(siteInfoDist))){
     disturbanceON = FALSE
-    siteInfoDist = matrix(0,nSites,4)
-    outDist = array(0,dim=c(nSites,max(nYearsMS),10))
+    siteInfoDist = matrix(0,multiSiteInit$nSites,4)
+    outDist = array(0,dim=c(multiSiteInit$nSites,max(nYearsMS),10))
   }else{
     disturbanceON = TRUE
     #siteInfoDist = matrix(0,nSites,4)
-    outDist = array(0,dim=c(nSites,max(nYearsMS),10))
+    outDist = array(0,dim=c(multiSiteInit$nSites,max(nYearsMS),10))
   }
 
   # if(length(startSimYear)==1) startSimYear <- rep(startSimYear,multiSiteInit$nSites)
@@ -977,12 +977,12 @@ reStartRegionPrebas <- function(multiSiteInit,
   ####initialize disturbance module if exists
   if(all(is.na(siteInfoDist))){
     disturbanceON = FALSE
-    siteInfoDist = matrix(0,nSites,4)
-    outDist = array(0,dim=c(nSites,max(nYearsMS),10))
+    siteInfoDist = matrix(0,multiSiteInit$nSites,4)
+    outDist = array(0,dim=c(multiSiteInit$nSites,max(nYearsMS),10))
   }else{
     disturbanceON = TRUE
     #siteInfoDist = matrix(0,nSites,4)
-    outDist = array(0,dim=c(nSites,max(nYearsMS),10))
+    outDist = array(0,dim=c(multiSiteInit$nSites,max(nYearsMS),10))
   }
   
   
