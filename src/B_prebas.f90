091150e0297dd8a67642e8c790fc40e9715c56f8
ForModLabUHel <francesco.minunno@helsinki.fi>
2024-02-07 15:21:41 +0000

    test

--- src/B_prebas.f90
+++ src/B_prebas.f90
@@ -168,7 +168,7 @@ ETSmean = ETSstart !initialise ETSmean using the starting value
  modOut(1,3,:,:) = output(1,3,:,:) ! assign site type and alfar
  modOut(2:nYears,3,:,:) = output(:,3,:,:) ! assign site type and alfar
  soilCtot(1) = sum(soilC(1,:,:,:)) !assign initial soilC
- if(modOut(1,1,1,1) ==1. ) write(1,*) modOut(1:nYears,3,:,:)
+ 
  do i = 1,nLayers
   modOut(:,4,i,1) = initVar(1,i)  ! assign species
   modOut(:,7,i,1) = initVar(2,i) ! assign initAge !age can be made species specific assigning different ages to different species
@@ -331,7 +331,7 @@ ETSmean = ETSmean + (ETS-ETSmean)/20.
 
 ! !calculate reneike and random mortality
 ! include 'mortalityCalc.h'
-
+if(modOut(1,1,1,1) ==1. ) write(1,*) modOut(1:nYears,3,:,:)
 do ij = 1 , nLayers 		!loop Species
  STAND=STAND_all(:,ij)
  species = int(max(1.,stand(4)))
