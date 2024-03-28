37a3e3ae772505e0f13a7f9ab39cb3d29d506f2e
jonathanholder <66249841+jonathanholder@users.noreply.github.com>
2024-03-28 09:31:30 +0200

    Revert "conversion of all tabs to two spaces in fortran code to avoid "Nonconforming tab character" warnings when compiling with gfortran on a mac"
    This reverts commit 64a6c8742d889a41f1278b572a87578ff9324352.

--- src/A_presel_f.f90
+++ src/A_presel_f.f90
@@ -43,16 +43,16 @@ subroutine fPheno_model(T, DOY, fS, t0,tcrit,tsumcrit,PhenoS,fPheno)
     endif
     
     if(PhenoS > tsumcrit - 0.005d0) then
-    fPheno = 1.d0
-  else 
-    fPheno = 0.d0
-  endif
+		fPheno = 1.d0
+	else 
+		fPheno = 0.d0
+	endif
     ! /* Quick solution to leaf out: 
      ! * After end of July we just apply season prediction based on conifer fS 
      ! *  for gradual leaf out. Assume leaves drop much faster that fS.
      ! *  ...essentially this should be light driven process...i think. */
     if(DOY > 212) then
-    fPheno = fS * fS
+ 	 fPheno = fS * fS
      if(fPheno < 0.5) fPheno = 0.d0
     endif
     
@@ -107,7 +107,7 @@ subroutine fCO2_model_mean(CO2,bCO2,fCO2)
    !parameters
    real(8), intent(in) :: bCO2
 
-  fCO2 = 1.d0 + bCO2 * log(CO2/380.d0) 
+	fCO2 = 1.d0 + bCO2 * log(CO2/380.d0) 
 end subroutine
 
 
@@ -122,15 +122,15 @@ subroutine fCO2_ET_model_mean(CO2,xCO2,fCO2_ET)
    !parameters
    real(8), intent(in) :: xCO2
 
-  fCO2_ET = 1.d0 + xCO2 * log(CO2/380.d0) 
+	fCO2_ET = 1.d0 + xCO2 * log(CO2/380.d0) 
 end subroutine
 
 
 ! /* GPP model, modified from Mäkelä et al 2008 */
 subroutine GPPfun(gpp, gpp380, ppfd, D, CO2, theta, &
-         fAPAR, fSsub, fD, fW, & 
-         fE,  pars) 
- !      // double *fE, FILE *flog, int LOGFLAG) {
+	       fAPAR, fSsub, fD, fW, & 
+	       fE,  pars) 
+ !		  // double *fE, FILE *flog, int LOGFLAG) {
  !  extern double fCO2_model_mean(double CO2, p2 b ) ;
  !   //    extern double fCO2_VPD_exponent(double CO2, double xCO2 ) ;
 
@@ -150,18 +150,18 @@ real(8) :: fEsub,fWsub,fLsub,fDsub
 real(8) :: soildepth,ThetaPWP,ThetaFC
 real(8) :: kappa,soilthres, gamma,beta,bCO2
 !initialise
-  soildepth = pars(1)
-  ThetaFC = pars(2)
-  ThetaPWP = pars(3)
-  kappa = pars(9)
-  soilthres = pars(11)
-  gamma = pars(10)
-  beta = pars(5)
+	soildepth = pars(1)
+	ThetaFC = pars(2)
+	ThetaPWP = pars(3)
+	kappa = pars(9)
+	soilthres = pars(11)
+	gamma = pars(10)
+	beta = pars(5)
     bCO2 = pars(12)
-  
-  thetavol = theta/soildepth
+	
+	thetavol = theta/soildepth
     REW=(thetavol-ThetaPWP)/(ThetaFC-ThetaPWP)
-  
+	
 
 !    /* Calculate first the reference condition (ca=380 ppm) effect */
     fDsub = min(exp(kappa * D),1.0d0)
@@ -172,10 +172,10 @@ real(8) :: kappa,soilthres, gamma,beta,bCO2
     else 
       if (REW < soilthres) then
         if (REW > 0.01) then
-      fWsub = REW/soilthres
-    else
-      fWsub = 0.0d0
-    endif
+			fWsub = REW/soilthres
+		else
+			fWsub = 0.0d0
+		endif
       else
         fWsub = 1.0d0
       endif
@@ -184,9 +184,9 @@ real(8) :: kappa,soilthres, gamma,beta,bCO2
     fLsub = 1.0d0 / (gamma * ppfd + 1.0d0)
 
     fEsub = min(fWsub,fDsub)
-  fW = fWsub
+	fW = fWsub
     fD = fEsub  !!!!here it should be reported fDsub 
-  fE = fEsub
+	fE = fEsub
 
     gpp380 = beta *  ppfd *  fAPAR * fSsub * fLsub * fEsub
     call fCO2_model_mean(CO2,bCO2,fCO2)
@@ -195,8 +195,8 @@ real(8) :: kappa,soilthres, gamma,beta,bCO2
     
     ! // if (LOGFLAG > 1.5) 
       ! // fprintf(flog, 
-        ! // "   gpp(): Modifiers: fAPAR %lf\tfSsub %lf\t fLsub %lf\t fDsub %lf\t fWsub %lf\tfEsub %lf\t fCO2 %lf\n                    gpp380 %lf\t gpp %lf\n",
-        ! // fAPAR, fSsub, fLsub, fDsub, fWsub, fEsub, fCO2, *gpp380, *gpp);
+	      ! // "   gpp(): Modifiers: fAPAR %lf\tfSsub %lf\t fLsub %lf\t fDsub %lf\t fWsub %lf\tfEsub %lf\t fCO2 %lf\n                    gpp380 %lf\t gpp %lf\n",
+	      ! // fAPAR, fSsub, fLsub, fDsub, fWsub, fEsub, fCO2, *gpp380, *gpp);
 
 
 
@@ -204,13 +204,13 @@ real(8) :: kappa,soilthres, gamma,beta,bCO2
     ! * CO2 effect not only influences fD but also fW, due to stomatal action
     
     ! fDCO2sub = fDsub * pow(exp(-0.4 * D),
-         ! fCO2_VPD_exponent(CO2, GPP_par.xCO2)) / exp(-0.4 * D) ;
+			   ! fCO2_VPD_exponent(CO2, GPP_par.xCO2)) / exp(-0.4 * D) ;
     ! fWCO2sub = fWsub * pow(fWsub, fCO2_VPD_exponent(CO2, GPP_par.xCO2));
 
     ! if (LOGFLAG > 1.5) 
       ! fprintf(flog, 
-        ! "   gpp(): Modifier values for GPP at %lf\n      fD=%lf\tfW=%lf\tfCO2mean=%lf\n", 
-        ! CO2, fDCO2sub, fWCO2sub, fCO2_model_mean(CO2, GPP_par.bCO2));
+	      ! "   gpp(): Modifier values for GPP at %lf\n      fD=%lf\tfW=%lf\tfCO2mean=%lf\n", 
+	      ! CO2, fDCO2sub, fWCO2sub, fCO2_model_mean(CO2, GPP_par.bCO2));
     
     ! if (fDCO2sub > fWCO2sub) fECO2sub=fWCO2sub; else fECO2sub = fDCO2sub;
     
@@ -225,7 +225,7 @@ end subroutine
 
 !/* Rain is snow below T > 0 C, and snow melts above O C. */
 subroutine SnowFun(T, rain, snow, SnowMelt, &
-  MeltCoeff, T_0 , CWmax,SnowThreshold)
+	MeltCoeff, T_0 , CWmax,SnowThreshold)
 
  implicit none
 
@@ -248,7 +248,7 @@ subroutine SnowFun(T, rain, snow, SnowMelt, &
   if (T > T_0) then  
     SnowMelt = MeltCoeff*(T-T_0)
   else 
-  SnowMelt=0
+	SnowMelt=0
   endif
   
   if(snow + NewSnow - SnowMelt < 0) then
@@ -257,12 +257,12 @@ subroutine SnowFun(T, rain, snow, SnowMelt, &
   else
     snow = snow + NewSnow - SnowMelt
   endif
-  
+	
 end subroutine
 
 !/*Interception is a fraction of daily rainfall, fraction depending on fAPAR*/
 subroutine interceptionfun(rain, intercepted, Temp, fAPAR, &
-  I_0 , SnowThreshold)
+	I_0 , SnowThreshold)
 
  implicit none
 
@@ -287,8 +287,8 @@ endsubroutine
  ! * and evapotranspiration. No drainage occurs below field capacity */
 subroutine swbalance(theta, throughfall, snowmelt, et, &
                tauDrainage,ThetaFC,soildepth, &
-         drainage, snow, canw, &
-         MeltCoeff, T_0 , CWmax,SnowThreshold)
+			   drainage, snow, canw, &
+			   MeltCoeff, T_0 , CWmax,SnowThreshold)
 
  implicit none
 
@@ -307,12 +307,12 @@ subroutine swbalance(theta, throughfall, snowmelt, et, &
   if (CWmax > 0.00000001) then 
     if ( (canw + snow - et) > 0 ) then             
       if ( (canw - et) > 0 ) then 
-    canw = canw -et
-    etfromvegandsoil = 0
+		canw = canw -et
+		etfromvegandsoil = 0
       elseif (canw - et < 0) then !// in this case, there's enough snow left
-    snow = snow + canw - et
-    canw = 0
-    etfromvegandsoil = 0
+		snow = snow + canw - et
+		canw = 0
+		etfromvegandsoil = 0
       endif    
     else
       etfromvegandsoil = et - canw - snow
@@ -356,19 +356,19 @@ subroutine swbalance(theta, throughfall, snowmelt, et, &
     ! /*
 ! if (st0 > sitepar.ThetaFC * sitepar.soildepth) {
       ! *drainage = (st0 - sitepar.ThetaFC * sitepar.soildepth) / 
-  ! sitepar.tauDrainage;      
+	! sitepar.tauDrainage;      
     ! }
     ! if (*drainage < (sitepar.ThetaFC * sitepar.soildepth - 
-         ! sitepar.ThetaPWP * sitepar.soildepth) / 
-  ! 10000) //pow(sitepar.tauDrainage, 5)) 
-  ! *drainage = (sitepar.ThetaFC * sitepar.soildepth - 
-         ! sitepar.ThetaPWP * sitepar.soildepth) / 
-    ! 10000; //pow(sitepar.tauDrainage, 5);
+		     ! sitepar.ThetaPWP * sitepar.soildepth) / 
+	! 10000) //pow(sitepar.tauDrainage, 5)) 
+	! *drainage = (sitepar.ThetaFC * sitepar.soildepth - 
+		     ! sitepar.ThetaPWP * sitepar.soildepth) / 
+	  ! 10000; //pow(sitepar.tauDrainage, 5);
     
     ! if (st0 <= sitepar.ThetaFC * sitepar.soildepth && 
-  ! st0 > sitepar.ThetaPWP * sitepar.soildepth) { 
+	! st0 > sitepar.ThetaPWP * sitepar.soildepth) { 
       ! *drainage = (st0 - sitepar.ThetaPWP * sitepar.soildepth) / 
-  ! 10000; //pow(sitepar.tauDrainage, 5);      
+	! 10000; //pow(sitepar.tauDrainage, 5);      
       ! *theta = st0 - *drainage;
     ! }
   
@@ -394,10 +394,10 @@ endsubroutine
  ! * for Rnet */
 subroutine ETfun(D, theta, ppfd, fAPAR, T, &
              beta, kappa,chi,soilthres,nu, &
-       soildepth, ThetaFC,ThetaPWP, &
+			 soildepth, ThetaFC,ThetaPWP, &
              canw, fE, A, fWgpp, fCO2mean, & 
-       xCO2, & !//double fCO2mean, 
-       CO2, etmodel, transp, evap, fWE, et) 
+			 xCO2, & !//double fCO2mean, 
+			 CO2, etmodel, transp, evap, fWE, et) 
 
   
  implicit none
@@ -419,7 +419,7 @@ subroutine ETfun(D, theta, ppfd, fAPAR, T, &
   thetavol = theta/soildepth
   REW=(thetavol-ThetaPWP)/(ThetaFC-ThetaPWP)
   ! //  double fEsub = -999; /* Minimum of fW and fD returned if ET-model 
-  ! //      * flag indicates similar modifier as for GPP */
+  ! //			* flag indicates similar modifier as for GPP */
   fWsub=1.d0
   ! //  double fDsub=1;
   cp = 1003.5 ! // J/(kg K) (nearly constant, this is dry air on sea level)
@@ -433,7 +433,7 @@ subroutine ETfun(D, theta, ppfd, fAPAR, T, &
 
   ! // rho=pressure/(R * (T+273.15) ); // Dry air density, kg/m3
   lambda = (-0.0000614342 * T**3 + 0.00158927 * T**2 - & 
-      2.36418 * T +  2500.79) * 1000. ! // J/kg
+	    2.36418 * T +  2500.79) * 1000. ! // J/kg
   psychom= cp * pressure / (lambda * MWratio) ! // Pa/C, wiki
   s = 1000. * 4098.0 * (0.6109 * exp((17.27 * T)/(T+237.3))) / & 
     ((T+237.3)**2)  !  // Pa/C! (Ice has nearly the same slope)
@@ -445,10 +445,10 @@ subroutine ETfun(D, theta, ppfd, fAPAR, T, &
   else
     if (REW < soilthres) then
       if (REW > 0.01) then
-     fWsub = REW/soilthres
-    else
-      fWsub = 0.0
-    endif
+	   fWsub = REW/soilthres
+	  else
+ 	   fWsub = 0.0
+	  endif
     else
       fWsub = 1.0;
     endif
@@ -468,7 +468,7 @@ subroutine ETfun(D, theta, ppfd, fAPAR, T, &
 
   ! // if (LOGFLAG > 1.5)
     ! // fprintf(flog, "   ETfun(): CO2mean=%lf\tat CO2=%lf\n", 
-      ! // fCO2mean, CO2);
+	    ! // fCO2mean, CO2);
   
   if (etmodel == -1) then
     transp = D * beta*A/(D**kappa) * &
@@ -501,17 +501,17 @@ subroutine ETfun(D, theta, ppfd, fAPAR, T, &
   endif
   if (etmodel == 2) then
       et = D * (1 + beta/(D**kappa)) * A / CO2 * & 
-  (fWgpp**nu) * & !// ET differently sensitive to soil water than GPP
-  fCO2mean + & !// Mean effect of CO2 on transpiration      
-  chi * (1-fAPAR) *  fWsub * ppfd
+	(fWgpp**nu) * & !// ET differently sensitive to soil water than GPP
+	fCO2mean + & !// Mean effect of CO2 on transpiration      
+	chi * (1-fAPAR) *  fWsub * ppfd
   endif
 
   ! // if (LOGFLAG > 2.5)
-    ! // fprintf(flog, "      ETfun(): Model=%d\nD\t%lf\nET_par.beta\t%lf\nA\t%lf\npow(D, ET_par.kappa)\t%lf\npow(fWgpp, ET_par.nu)\t%lf\nfWgpp\t%lf\nET_par.nu\t%lf\nfCO2mean\t%lf\nCO2\t%lf\nET_par.chi\t%lf\ns/(s+psychom)\t%lf\n1-fAPAR\t%lf\nfWsum\t%lf\nppfd\t%lf\n-->et\t%lf\n",      
-      ! // etmodel, D, ET_par.beta, A, pow(D, ET_par.kappa), 
-      ! // pow(fWgpp, ET_par.nu), fWgpp, ET_par.nu,
-      ! // fCO2mean, 
-      ! // CO2,
-      ! // ET_par.chi ,   s / (s + psychom), 1-fAPAR, fWsub,  ppfd, et);
+    ! // fprintf(flog, "      ETfun(): Model=%d\nD\t%lf\nET_par.beta\t%lf\nA\t%lf\npow(D, ET_par.kappa)\t%lf\npow(fWgpp, ET_par.nu)\t%lf\nfWgpp\t%lf\nET_par.nu\t%lf\nfCO2mean\t%lf\nCO2\t%lf\nET_par.chi\t%lf\ns/(s+psychom)\t%lf\n1-fAPAR\t%lf\nfWsum\t%lf\nppfd\t%lf\n-->et\t%lf\n",	    
+	    ! // etmodel, D, ET_par.beta, A, pow(D, ET_par.kappa), 
+	    ! // pow(fWgpp, ET_par.nu), fWgpp, ET_par.nu,
+	    ! // fCO2mean, 
+	    ! // CO2,
+	    ! // ET_par.chi ,   s / (s + psychom), 1-fAPAR, fWsub,  ppfd, et);
 
 endsubroutine
