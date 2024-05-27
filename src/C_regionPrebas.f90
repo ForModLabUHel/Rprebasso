8a3532a8a04bc5db669dc98e0d4204670110dc8f
jonathanholder <66249841+jonathanholder@users.noreply.github.com>
2024-05-27 16:21:04 +0300

    troubleshoot, pt.3

--- src/C_regionPrebas.f90
+++ src/C_regionPrebas.f90
@@ -219,6 +219,7 @@ call prioDistInSO(outDist(:, (ij-1), :), nSites, maxYears, ij, siteOrder(:,:))
    endif
    domSp = maxloc(multiOut(i,ij,13,1:jj,1)) ! find dominant spec (BA)
    layerX = int(domSp(1))
+   outDist(i,ij,10) = domSp(1) !testing
 
   if(multiOut(i,ij,7,layerX,1) > ageMitigScen) then ! dominant layer age (=stand age) overt threshold
     ! disable all management induced by disturbance
