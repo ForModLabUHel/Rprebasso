a9a41084307c0a3eef1da1160c6f12521be3adb6
ForModLabUHel <francesco.minunno@helsinki.fi>
2025-01-31 15:23:23 +0000

    gpp threshold for start of the season calculation, p0 aproximation

--- src/A_routines.f90
+++ src/A_routines.f90
@@ -531,7 +531,7 @@ fAPARc = fAPAR
 		day, &!!!!this is DOY
 		transp, evap, fWE,CO2model)
 
- call SMIfromPRELES(GPP,fW,prelesOut(7))
+ call SMIfromPRELES(GPP,fW,prelesOut(7),sum(fAPARc)/365.)
 
 prelesOut(1) = sum(GPP(1:nDays))
 prelesOut(2) = sum(ET(1:nDays))
@@ -2324,18 +2324,20 @@ endsubroutine
 
 
 !!calculate the soil moisture index to be used in the bark beatle disturbance calculations
-subroutine SMIfromPRELES(GPP,fW,SMI)
-  real (kind=8), intent(in) :: GPP(365),fW(365)
+subroutine SMIfromPRELES(GPP,fW,SMI,fAPAR)
+  real (kind=8), intent(in) :: GPP(365),fW(365),fAPAR
   real (kind=8), intent(out) :: SMI
   real(kind=8) :: gpp_threshold
   integer :: startSeason, endSeason
   
-  gpp_threshold=5.d0 !!!!gpp threshold that should be considered for start and end of the season
+  !gpp threshold that should be considered for start and end of the season
+  gpp_threshold=5.0d0 *fAPAR !!! it is divided by fAPAR to aproximate to p0
   
   startSeason = 1
   do while (GPP(startSeason) <= gpp_threshold .and. startSeason < 366)
     startSeason = startSeason + 1
   enddo
+  
   
   endSeason = 365
   do while (GPP(endSeason) <= gpp_threshold .and. endSeason > 1)
