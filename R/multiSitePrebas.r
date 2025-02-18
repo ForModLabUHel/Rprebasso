2088a9c6510f0f3fea9289a2e4dc5187ab3013be
ForModLabUHel <francesco.minunno@helsinki.fi>
2025-02-18 16:28:31 +0000

    update parameters clcut

--- R/multiSitePrebas.r
+++ R/multiSitePrebas.r
@@ -307,12 +307,12 @@ if(all(is.na(TsumSBBs))) TsumSBBs <- matrix(-999,nSites,4) #wdimpl
         c(ClCutD_Pine(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
           ClCutD_Spruce(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
           ClCutD_Birch(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
-          NA,NA,NA,NA,NA,NA,NA,NA)  ###"fasy","pipi","eugl","rops","popu",'eugrur','piab(DE)','quil')
+          NA,NA,NA,NA,NA,NA,NA,NA,NA)  ###"fasy","pipi","eugl","rops","popu",'eugrur','piab(DE)','quil')
     if(ClCut[i]==1 & all(is.na(inAclct[i,]))) inAclct[i,] <-
         c(ClCutA_Pine(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
           ClCutA_Spruce(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
           ClCutA_Birch(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
-          80,50,13,30,50,13,120,100)  ###"fasy","pipi","eugl","rops","popu",'eugrur','piab(DE)','quil')
+          80,50,13,30,50,13,120,100,80)  ###"fasy","pipi","eugl","rops","popu",'eugrur','piab(DE)','quil')
     if(any(!is.na(inDclct[i,]))) inDclct[i,is.na(inDclct[i,])] <- max(inDclct[i,],na.rm=T)
     if(all(is.na(inDclct[i,]))) inDclct[i,] <- 9999999.99
     if(any(!is.na(inAclct[i,]))) inAclct[i,is.na(inAclct[i,])] <- max(inAclct[i,],na.rm=T)
