05f97835e3b430a98c197b60e9744cd1b7b5aa05
ForModLabUHel <francesco.minunno@helsinki.fi>
2025-05-08 15:50:08 +0100

    update BA seedling initialization

--- R/parameters.r
+++ R/parameters.r
@@ -4,7 +4,7 @@ pCO2model[1,] <- c(0.5,-0.364)
 pCO2model[2,] <- c(2000,0.4)
 
 ###default initial state at plantation
-initSeedling.def <- c(1.31,0.5,0.0431969,0.2,NA)
+initSeedling.def <- c(1.31,0.5,0.07343473,0.2,NA)
 names(initSeedling.def) <- c("H","dbh","BA","hc","Ac")
 
 
