521d9965b520ada40c608940c66b11542f43b32d
ForModLabUHel <francesco.minunno@helsinki.fi>
2025-12-02 15:15:07 +0000

    add authomatically tauDrainage if not exists in  siteInfo

--- R/multiSitePrebas.r
+++ R/multiSitePrebas.r
@@ -198,6 +198,10 @@ if(all(is.na(TsumSBBs))) TsumSBBs <- matrix(-999,nSites,4) #wdimpl
     siteInfo = matrix(c(1,1,3,160,0,0,20,3,3,413.,0.45,0.118,3),nSites,13,byrow = T) ###default values for nspecies and site type = 3
     siteInfo[,1] <- 1:nSites
   }
+  ### automatically add tauDrainage if missing ###
+  if(dim(siteInfo)[2]==12) siteInfo <- cbind(siteInfo,pPRELES[4])
+  ### --- ###  
+  
   
   if(ingrowth){
     ingrowthStep <- 25
