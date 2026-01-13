70f0a630998d8ff22d3b3658b85909224fc89941
ForModLabUHel <francesco.minunno@helsinki.fi>
2026-01-13 13:58:14 +0000

    flexible parameters clcut

--- R/multiSitePrebas.r
+++ R/multiSitePrebas.r
@@ -328,16 +328,18 @@ if(all(is.na(TsumSBBs))) TsumSBBs <- matrix(-999,nSites,4) #wdimpl
   multiETS[which(is.na(multiETS))] <- 0.
   ####process clearcut
   for(i in 1: nSites){
-    if(ClCut[i]==1 & all(is.na(inDclct[i,]))) inDclct[i,] <-
-        c(ClCutD_Pine(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
-          ClCutD_Spruce(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
-          ClCutD_Birch(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
-          NA,NA,NA,NA,NA,NA,NA,NA)  ###"fasy","pipi","eugl","rops","popu",'eugrur','piab(DE)','quil')
-    if(ClCut[i]==1 & all(is.na(inAclct[i,]))) inAclct[i,] <-
-        c(ClCutA_Pine(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
-          ClCutA_Spruce(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
-          ClCutA_Birch(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
-          80,50,13,30,50,13,120,100)  ###"fasy","pipi","eugl","rops","popu",'eugrur','piab(DE)','quil')
+    if(ClCut[i]==1 & all(is.na(inDclct[i,]))){
+      inDclct[i,] <- inDclct_def[1:allSp]
+      inDclct[i,1:3] <-  c(ClCutD_Pine(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]), #update for pine, spruce and birch in Finland
+                           ClCutD_Spruce(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
+                           ClCutD_Birch(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]))} 
+    
+    if(ClCut[i]==1 & all(is.na(inAclct[i,]))){
+      inAclct[i,] <- inAclct_def[1:allSp]
+      inAclct[i,1:3] <- c(ClCutA_Pine(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]), #update for pine, spruce and birch in Finland
+                          ClCutA_Spruce(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]),
+                          ClCutA_Birch(ETSmean[climIDs[i]],ETSthres,siteInfo[i,3]))} 
+    
     if(any(!is.na(inDclct[i,]))) inDclct[i,is.na(inDclct[i,])] <- max(inDclct[i,],na.rm=T)
     if(all(is.na(inDclct[i,]))) inDclct[i,] <- 9999999.99
     if(any(!is.na(inAclct[i,]))) inAclct[i,is.na(inAclct[i,])] <- max(inAclct[i,],na.rm=T)
--- R/prebas.r
+++ R/prebas.r
@@ -363,18 +363,19 @@ prebas <- function(nYears,
     if(is.na(inDclct)) inDclct <- 9999999.99
     if(is.na(inAclct)) inAclct <- 9999999.99
   }
-  # if(ClCut==1 & all(is.na(initVar)) & is.na(inDclct)) inDclct <-
-  if(ClCut==1 & all(is.na(inDclct))) inDclct <-
-    c(ClCutD_Pine(ETSmean,ETSthres,siteInfo[3]), ####pine in Finland
-      ClCutD_Spruce(ETSmean,ETSthres,siteInfo[3]), ####spruce in Finland
-      ClCutD_Birch(ETSmean,ETSthres,siteInfo[3]), ####birch in Finland
-      NA,NA,NA,NA,NA)  ###"fasy","pipi","eugl","rops"
-  # if(ClCut==1 & all(is.na(initVar)) & is.na(inAclct)) inAclct <-
-  if(ClCut==1 & all(is.na(inAclct))) inAclct <-
-    c(ClCutA_Pine(ETSmean,ETSthres,siteInfo[3]),   ####pine in Finland
-      ClCutA_Spruce(ETSmean,ETSthres,siteInfo[3]), ####spruce in Finland
-      ClCutA_Birch(ETSmean,ETSthres,siteInfo[3]),  ####birch in Finland
-      80,50,13,30,80)  ###"fasy","pipi","eugl","rops"
+  if(ClCut==1 & all(is.na(inDclct))){
+    inDclct <- inDclct_def
+    inDclct <- c(ClCutD_Pine(ETSmean,ETSthres,siteInfo[3]), ####pine in Finland
+                 ClCutD_Spruce(ETSmean,ETSthres,siteInfo[3]), ####spruce in Finland
+                 ClCutD_Birch(ETSmean,ETSthres,siteInfo[3])) ####birch in Finland
+  } 
+  
+  if(ClCut==1 & all(is.na(inAclct))){
+    inAclct <- inAclct_def
+    inAclct <- c(ClCutA_Pine(ETSmean,ETSthres,siteInfo[3]),   ####pine in Finland
+                 ClCutA_Spruce(ETSmean,ETSthres,siteInfo[3]), ####spruce in Finland 
+                 ClCutA_Birch(ETSmean,ETSthres,siteInfo[3]))  ####birch in Finland
+  } 
   if(any(is.na(inDclct))) inDclct[which(is.na(inDclct))] <- 9999999.99
   if(length(inDclct)==1) inDclct<- rep(inDclct,nSp)
   if(any(is.na(inAclct))) inAclct[which(is.na(inAclct))] <- 9999999.99
